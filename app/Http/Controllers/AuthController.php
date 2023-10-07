b54e7fd88f93e16d438303604b34a8c44f0549cc
Thiago Varallo <thiagovaralllo@gmail.com>
2023-10-07 00:49:50 -0300

    add AuthController method login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'require|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorizad',
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+            ]
+        ]);
+    }
+}
